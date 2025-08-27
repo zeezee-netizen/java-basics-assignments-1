6bfb84a28b8a77dc439695188d0749ec968f34ae
zeezee-netizen <zeezee900812@gmail.com>
2025-08-27 22:31:56 +0800

    Update MathOps.java

--- 04-methods/MathOps.java
+++ 04-methods/MathOps.java
@@ -1,12 +1,33 @@
 package mathOps;
 
+//File: MathOps.java
 public class MathOps {
-    public static int add(int a, int b) {
-        return a + b;
-    }
+ // Addition
+ public static int add(int a, int b) {
+     return a + b;
+ }
 
-    public static void main(String[] args) {
-        int sum = add(5, 7);
-        System.out.println("Sum = " + sum);
-    }
+ // Subtraction
+ public static int subtract(int a, int b) {
+     return a - b;
+ }
+
+ // Multiplication
+ public static int multiply(int a, int b) {
+     return a * b;
+ }
+
+ public static void main(String[] args) {
+     int x = 12;
+     int y = 4;
+
+     int sum = add(x, y);
+     int diff = subtract(x, y);
+     int product = multiply(x, y);
+
+     System.out.println("Given x=" + x + ", y=" + y);
+     System.out.println("Sum = " + sum);
+     System.out.println("Difference = " + diff);
+     System.out.println("Product = " + product);
+ }
 }
