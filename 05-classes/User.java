bcb8da92eee0a3ba65b7d695050413f6c60c66a5
zeezee-netizen <zeezee900812@gmail.com>
2025-08-27 22:32:28 +0800

    Update User.java

--- 05-classes/User.java
+++ 05-classes/User.java
@@ -1,15 +1,34 @@
 package classes;
 
+//File: User.java
 public class User {
-    public String username;
-    private String password;
+ public String username;
+ private String password;
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
+ // Constructor
+ public User(String username, String password) {
+     this.username = username;
+     setPassword(password); // use setter to enforce validation
+ }
 
-    public boolean canLogin(String inputPwd) {
-        return this.password.equals(inputPwd);
-    }
+ // Getter for password (usually avoided in real apps, but for demo)
+ public String getPassword() {
+     return password;
+ }
+
+ // Setter with validation: min length = 6
+ public boolean setPassword(String newPassword) {
+     if (newPassword.length() >= 6) {
+         this.password = newPassword;
+         return true;
+     } else {
+         System.out.println("Password must be at least 6 characters.");
+         return false;
+     }
+ }
+
+ // Behaviour method
+ public boolean canLogin(String inputPwd) {
+     return this.password.equals(inputPwd);
+ }
 }
